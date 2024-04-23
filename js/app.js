d5932ceb5daaab866fdeb84d8d15e3ec9a4b3570
mariyumsohail <155555622+mariyumsohail@users.noreply.github.com>
2024-04-23 19:32:25 +0500

    Add files via upload
    Alerts

---
+++ js/app.js
@@ -0,0 +1,8 @@
+alert("alert");
+alert("Error! please enter a valid password");
+alert("Welcome to JS land..\n Happy coding!");
+alert("Wlcome To JS land..");
+alert("Prevent! this page from creating the additional dialogs");
+var text = "Hello ! I can run JS through my web brouser's console";
+console.log(text);
+alert("practice more");
